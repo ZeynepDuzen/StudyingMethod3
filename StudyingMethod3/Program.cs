dc68c56b3827b37884cbeff0df38afbfc4ee94de
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-19 06:04:52 +0300

    Proje dosyası ekle.

---
+++ StudyingMethod3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyingMethod3
+{
+    internal class Program
+    {
+        static void MyMethod()
+        {
+            Console.WriteLine("I created a method");
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod();
+            MyMethod();
+            MyMethod();
+            MyMethod();
+            MyMethod();
+
+            Console.Read();
+        }
+    }
+}
